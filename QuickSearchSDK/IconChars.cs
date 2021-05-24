b305fab726a59e47b7540b34c5220fa8fc1b84c5
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-05-24 05:37:59 +0200

    added doc comments

--- QuickSearchSDK/IconChars.cs
+++ QuickSearchSDK/IconChars.cs
@@ -8,37 +8,69 @@ namespace QuickSearch
 {
     static class IconChars
     {
+        /// <summary>Settings Icon</summary>
         public const char Settings = '';
+        /// <summary>Question mark Icon</summary>
         public const char Help = '';
+        /// <summary>Chain Icon</summary>
         public const char Link = '';
+        /// <summary>i Icon</summary>
         public const char Info = '';
+        /// <summary>Globe Icon</summary>
         public const char Globe = '';
+        /// <summary>World Icon</summary>
         public const char World = '';
+        /// <summary>Web Icon</summary>
         public const char Web = '';
+        /// <summary>Star Icon</summary>
         public const char Star = '';
+        /// <summary>Ticked box Icon</summary>
         public const char TickedBox = '';
+        /// <summary>Toggle in on state Icon</summary>
         public const char ToggleOn = '';
+        /// <summary>Toggle in off state Icon</summary>
         public const char ToggleOff = '';
+        /// <summary>Shopping cart Icon</summary>
         public const char ShoppingCart = '';
+        /// <summary>Crossed arrows Icon</summary>
         public const char Shuffle = '';
+        /// <summary>Power Icon</summary>
         public const char Power = '';
+        /// <summary>Plus Icon</summary>
         public const char Plus = '';
+        /// <summary>Globe with News Icon</summary>
         public const char News = '';
+        /// <summary>Maximize Icon</summary>
         public const char Maximize = '';
+        /// <summary>Emptpy battery Icon</summary>
         public const char BatteryEmpty = '\ueeb1';
+        /// <summary>Low battery Icon</summary>
         public const char BatteryLow = '\ueeb4';
+        /// <summary>Half full battery Icon</summary>
         public const char BatteryHalf = '\ueeb3';
+        /// <summary>Full battery Icon</summary>
         public const char BatteryFull = '\ueeb2';
+        /// <summary>Alarm bell Icon</summary>
         public const char AlarmBell = '\ueea3';
+        /// <summary>Magnifying glass Icon</summary>
         public const char Search = '\ued11';
+        /// <summary>Magnifying glass on folder Icon</summary>
         public const char FolderSearch = '\ued14';
-        public const char PlayStation = '\ue925';
+        /// <summary>Playstation Icon</summary>
+        public const char Playstation = '\ue925';
+        /// <summary>Retro console Icon</summary>
         public const char GameConsole = '\uea2f';
+        /// <summary>Music note Icon</summary>
         public const char Music = '\ueca3';
+        /// <summary>Speech bubble Icon</summary>
         public const char Message = '\uec68';
+        /// <summary>Exit Icon</summary>
         public const char Exit = '\uEF7C';
+        /// <summary>Refresh Icon</summary>
         public const char Refresh = '\uEFD1';
+        /// <summary>Zpped folder Icon</summary>
         public const char Zipped = '\uF02E';
+        /// <summary>Terminal Icon</summary>
         public const char Terminal = '\uF00A';
     }
 }
