c541cead4844ac269f916f855349b1cb310d5822
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-05-24 05:26:04 +0200

    added icon char constants

---
+++ QuickSearchSDK/IconChars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSearch
+{
+    static class IconChars
+    {
+        public const char Settings = '';
+        public const char Help = '';
+        public const char Link = '';
+        public const char Info = '';
+        public const char Globe = '';
+        public const char World = '';
+        public const char Web = '';
+        public const char Star = '';
+        public const char TickedBox = '';
+        public const char ToggleOn = '';
+        public const char ToggleOff = '';
+        public const char ShoppingCart = '';
+        public const char Shuffle = '';
+        public const char Power = '';
+        public const char Plus = '';
+        public const char News = '';
+        public const char Maximize = '';
+        public const char BatteryEmpty = '\ueeb1';
+        public const char BatteryLow = '\ueeb4';
+        public const char BatteryHalf = '\ueeb3';
+        public const char BatteryFull = '\ueeb2';
+        public const char AlarmBell = '\ueea3';
+        public const char Search = '\ued11';
+        public const char FolderSearch = '\ued14';
+        public const char PlayStation = '\ue925';
+        public const char GameConsole = '\uea2f';
+        public const char Music = '\ueca3';
+        public const char Message = '\uec68';
+        public const char Exit = '\uEF7C';
+        public const char Refresh = '\uEFD1';
+        public const char Zipped = '\uF02E';
+    }
+}
