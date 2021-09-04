e9eb41e9f38fcbac3dfd33fcb31a3b3980b2cdc9
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-09-04 21:41:45 +0200

    added new strings

--- source/Settings/SearchSettings.cs
+++ source/Settings/SearchSettings.cs
@@ -52,59 +52,59 @@ namespace QuickSearch
             }
             return false;
         }
-        [GenericOption("Search Hotkey")]
+        [GenericOption("LOC_QS_LocalHotkeyShort")]
         public Hotkey SearchShortcut { get; set; } = new Hotkey(Key.F, ModifierKeys.Control);
-        [GenericOption("Global Search Hotkey")]
+        [GenericOption("LOC_QS_GlobalHotkeyShort")]
         public Hotkey SearchShortcutGlobal { get; set; } = new Hotkey(Key.F, ModifierKeys.Control | ModifierKeys.Alt);
         public string HotkeyText { get; set; } = string.Empty;
         public string HotkeyTextGlobal { get; set; } = string.Empty;
 
-        [NumberOption("Search Threshold", Min = 0, Max = 1, Tick = 0.01f)]
+        [NumberOption("LOC_QS_SearchThresholdShort", Min = 0, Max = 1, Tick = 0.01f)]
         public double Threshold { get; set; } = 0.55;
-        [GenericOption("Expand all items", Description = "If enabled, always show more detailed version of items.")]
+        [GenericOption("LOC_QS_ExpandAllItems")]
         public bool ExpandAllItems 
         {
             get => expandAllItems;
             set { expandAllItems = value; plugin?.searchWindow?.UpdateListBox(plugin.searchWindow.SearchResults.Items.Count, true); }
         }
-        [GenericOption("Show Seperator", Description = "If enabled, show line inbetween items.")]
+        [GenericOption("LOC_QS_ShowSeparator")]
         public bool ShowSeperator { get; set; } = false;
-        [GenericOption("Add Items With Lower Priority")]
+        [GenericOption("LOC_QS_LowerPriorityAddingShort")]
         public bool IncrementalUpdate { get; set; } = false;
-        [NumberOption("Maximum number of results", Min = 0, Tick = 1)]
+        [NumberOption("LOC_QS_MaxResults", Min = 0, Tick = 1)]
         public int MaxNumberResults { get; set; } = 20;
-        [GenericOption("Enable External GameActions")]
+        [GenericOption("LOC_QS_AllowExternalGameActions")]
         public bool EnableExternalGameActions { get; set; } = true;
-        [GenericOption("Enable External SearchItems")]
+        [GenericOption("LOC_QS_AllowExternalItems")]
         public bool EnableExternalItems { get; set; } = true;
-        [NumberOption("Delay until requesting async items", Min = 10, Max = 1000, Tick = 10f)]
+        [NumberOption("LOC_QS_AsyncDelayShort", Min = 10, Max = 1000, Tick = 10f)]
         public int AsyncItemsDelay { get; set; } = 500;
         public SortedDictionary<string, AssemblyOptions> EnabledAssemblies { get; set; } = new SortedDictionary<string, AssemblyOptions>();
         public float PrioritizationThreshold { get; set; } = 0.55f;
         public int MaxPrioritizedGames { get; set; } = 1;
-        [GenericOption("Prioritize Installation Status for Sorting")]
+        [GenericOption("LOC_QS_PrioritizeInstallationStatusShort")]
         public bool InstallationStatusFirst { get; set; } = true;
-        [NumberOption("ITAD Threshold", Min = 0, Max = 1, Tick = 0.01f)]
+        [NumberOption("LOC_QS_ITADThresholdShort", Min = 0, Max = 1, Tick = 0.01f)]
         public float ITADThreshold { get; set; } = 0.75f;
         public string ITADOverride { get; set; } = "+";
-        [GenericOption("Enable ITAD Search")]
+        [GenericOption("LOC_QS_EnableITAD")]
         public bool ITADEnabled { get; set; } = true;
         public SortedDictionary<string, ITADShopOption> EnabledITADShops { get; set; } = new SortedDictionary<string, ITADShopOption>();
-        [GenericOption("Glass Effect", Description = "If enabled, blur background under the search window.")]
+        [GenericOption("LOC_QS_GlassEffectShort", Description = "LOC_QS_GlassEffect")]
         public bool EnableGlassEffect { get => enableGlassEffect; set { enableGlassEffect = value; if (value) SearchPlugin.Instance?.EnableGlassEffect(); else SearchPlugin.Instance?.DisableGlassEffect(); } }
         private bool enableGlassEffect = true;
-        [NumberOption("Border Thickness", Min = 0, Max = 30, Tick = 1)]
+        [NumberOption("LOC_QS_BorderWidthShort", Min = 0, Max = 30, Tick = 1)]
         public int OuterBorderThickness
         {
             get => outerBorderThickness;
             set { outerBorderThickness = value; plugin?.UpdateBorder(value); plugin?.searchWindow?.UpdateListBox(plugin.searchWindow.SearchResults.Items.Count, true); }
         }
-        [GenericOption("Enable Filter-SubItemSources")]
+        [GenericOption("LOC_QS_FilterSubItems")]
         public bool EnableFilterSubSources { get; set; } = true;
-        [GenericOption("Enable Global Hotkey")]
+        [GenericOption("LOC_QS_GlobalHotkeyToggle")]
         public bool EnableGlobalHotkey { get => enableGlobalHotkey; set { enableGlobalHotkey = value; if (value) SearchPlugin.Instance?.RegisterGlobalHotkey(); else SearchPlugin.Instance?.UnregisterGlobalHotkey(); } }
         private bool enableGlobalHotkey = false;
-        [GenericOption("Prefer cover art", Description = "If available, use the cover art instead of the game icon as the icon.")]
+        [GenericOption("LOC_QS_PreferCoverArt")]
         public bool PreferCoverArt { get; set; } = false;
 
         public delegate void SettingsChangedHandler(SearchSettings newSettings, SearchSettings oldSettings);
