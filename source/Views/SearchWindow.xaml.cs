9592eca46fa5c7787ffaaee7ef8bd99a0d841671
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-10-11 23:33:57 +0200

    fix: loop not parallelized

--- source/Views/SearchWindow.xaml.cs
+++ source/Views/SearchWindow.xaml.cs
@@ -317,7 +317,7 @@ namespace QuickSearch
                         .ToArray();
                     } else
                     {
-                        canditates = searchItems.Concat(queryDependantItems)
+                        canditates = searchItems.Concat(queryDependantItems).AsParallel()
                         .Where(item => !cancellationToken.IsCancellationRequested && ComputePreliminaryScore(item, input) >= searchPlugin.Settings.Threshold)
                         .Select(item => new Candidate { Marked = false, Item = item, Score = ComputeScore(item, input) })
                         .Where(candidate => candidate.Score >= searchPlugin.Settings.Threshold)
